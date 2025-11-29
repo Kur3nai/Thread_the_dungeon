5d7102408229f2640b3eacd82048defacbfef593
Kur3nai <darkserr0@gmail.com>
2025-11-29 23:41:56 +0800

    Player Animation and Torch

--- thread/scripts/shady_x.gd
+++ thread/scripts/shady_x.gd
@@ -2,13 +2,15 @@ extends CharacterBody2D
 
 @onready var pick_up: Label = $pick_up
 @onready var torch: Sprite2D = $Light
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _ready() -> void:
-	torch.visible = false
+	torch.visible = true
 	pick_up.visible = false
+	animation_player.play("Torch")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
