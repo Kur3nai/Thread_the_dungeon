b1007db438ab4453ab41b263cf1ceadc73b25eda
limcheexian728-shadyx <cheexianlim96@gmail.com>
2025-11-29 12:53:01 +0800

    Torch System
    There's a "A and "D" movement and "F" Interact in Project Setting -> Input Map.
    When player node enter light and lamp area2d, words will come out. When Interact button pressed then light area 2d will disappear [queue_free()] and a chill node call "Torch" in player tscn will be set as .visible = true[was false in func onready].
    When entered the lamp area2d and player press the Interact button, the "Torch" chill node in player tscn visible will be set as false, and the "Torch" chill node in lamp tscn visible will be set as true

---
+++ thread/scripts/lamp.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var player_in_area = false
+var player:Node = null
+@onready var torch: Sprite2D = $Torch
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	torch.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in_area and Input.is_action_just_pressed("Interact"):
+		var player_torch = player.get_node("Torch")
+		if player_torch.visible:
+			torch.visible = true
+			player_torch.visible = false
+		else:
+			player.show_message("You need a torch")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ShadyX"):
+		if torch.visible:
+			body.hide_message()
+		else:  
+			player = body
+			body.show_message("Press F to insert the light")
+			player_in_area = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("ShadyX"):
+		player = null
+		body.hide_message()
+		player_in_area = false
---
+++ thread/scripts/shady_x.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@onready var pick_up: Label = $pick_up
+@onready var torch: Sprite2D = $Torch
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _ready() -> void:
+	torch.visible = false
+	pick_up.visible = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("MoveLeft", "MoveRight")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+func show_message(text):
+	$pick_up.text = text
+	$pick_up.show()
+
+func hide_message():
+	$pick_up.hide()
