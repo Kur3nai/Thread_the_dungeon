90929a38ad479f194df755ab40baad9c262ea868
Kur3nai <darkserr0@gmail.com>
2025-11-30 00:36:11 +0800

    Door

--- thread/scripts/door.gd
+++ thread/scripts/door.gd
@@ -2,11 +2,9 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	monitoring = false
-	visible =false
-	animation_player.animation_finished.connect(_on_animation_finished)
+	visible = false
 
 func _process(delta: float) -> void:
 	var door_linked_lamp = get_tree().current_scene.get_node("Door_linked_lamp")
@@ -15,18 +13,18 @@ func _process(delta: float) -> void:
 	if linked_lamp_torch.visible:
 		monitoring = true
 		visible = true
-		if not animation_player.is_playing():       
+		if not animation_player.is_playing() or animation_player.current_animation != "Door_Open":
 			animation_player.play("Door_Open")
 	else:
 		monitoring = false
 		visible = false
-		pass
-
-# This tiny extra function freezes the door on the last frame
-func _on_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Door_Open":
-		animation_player.pause()  
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ShadyX"):
 		get_tree().reload_current_scene()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Door_Open":
+		animation_player.stop()
+	pass # Replace with function body.
