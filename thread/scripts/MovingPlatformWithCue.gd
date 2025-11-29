22b3e6738c962573bace378a14f540a2099ac543
Kur3nai <darkserr0@gmail.com>
2025-11-30 03:49:47 +0800

    Fixes

--- thread/scripts/MovingPlatformWithCue.gd
+++ thread/scripts/MovingPlatformWithCue.gd
@@ -9,11 +9,12 @@ extends Path2D
 
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var opening_bell_421471: AudioStreamPlayer2D = $OpeningBell421471
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not loop:
-		animation_player.play("MovingPlatform")
+		animation_player.play("A_to_B")
 		animation_player.speed_scale = speed_scale
 		# Even if it's an animation, we might want it to wait!
 		if not move_platform:
@@ -25,3 +26,7 @@ func _process(delta: float) -> void:
 	# If move_platform is set to be true
 	if move_platform:
 		path_follow_2d.progress += speed
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	opening_bell_421471.play()
