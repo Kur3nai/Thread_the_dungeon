3a605c721ebec22fac42aca37d42795bdc7387eb
Albert <137798889+IRIDIUM86@users.noreply.github.com>
2025-11-30 01:17:05 +0800

    Update MovingPlatform.gd

--- thread/scripts/MovingPlatform.gd
+++ thread/scripts/MovingPlatform.gd
@@ -3,30 +3,25 @@ extends Path2D
 @export var loop = true
 @export var speed = 2.0
 @export var speed_scale = 1.0
-@export var is_activated: bool = false:
-	set = _set_activated
-	
+
+# Export move_platform variable
+@export var move_platform = true
+
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-signal move_platform
-
-# This function runs every time 'is_activated' changes
-func _set_activated(new_value: bool):
-	is_activated = new_value # Update the actual variable first
-	if is_activated == true:
-		# Call the movement function!
-		start_movement()
-		
-func start_movement() -> void:
-	if not loop:
-		animation_player.play("move_platform")
-		animation_player.speed_scale = speed_scale
-		set_process(false)
-		
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Just setting up the initial state, not starting the animation yet
-	# Example: print("Platform is armed and waiting!")
-	pass
+	if not loop:
+		animation_player.play("MovingPlatform")
+		animation_player.speed_scale = speed_scale
+		# Even if it's an animation, we might want it to wait!
+		if not move_platform:
+			animation_player.stop()
+		set_process(false)
 		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# If move_platform is set to be true
+	if move_platform:
+		path_follow_2d.progress += speed
