7e5a1a59cd8a7a4f70e2c4ea441c172e3f16b7fb
limcheexian728-shadyx <cheexianlim96@gmail.com>
2025-11-30 12:47:17 +0800

    Added letters

---
+++ thread/scripts/text_interacter.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var player_in_area = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in_area and Input.is_action_just_pressed("Interact"):
+		var letter = get_tree().current_scene.get_node("Letter")
+		letter.visible = true
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ShadyX"):
+		player_in_area = true
+		body.show_message("Press E to pick up the letter")
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("ShadyX"):
+		player_in_area = false
+		body.hide_message()
