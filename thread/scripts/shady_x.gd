96929cd6c49a8f3bdb20eb579c6106d822e59ab9
Kur3nai <darkserr0@gmail.com>
2025-11-30 13:18:38 +0800

    Fixes

--- thread/scripts/shady_x.gd
+++ thread/scripts/shady_x.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @onready var torch: Sprite2D = $Torch
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0
 
 func _ready() -> void:
 	torch.visible = false
