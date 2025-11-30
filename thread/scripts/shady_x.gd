57f3c70123bbb8812b1e52b6715a5d3932ec37bb
Kur3nai <darkserr0@gmail.com>
2025-11-30 08:46:18 +0800

    Fixing

--- thread/scripts/shady_x.gd
+++ thread/scripts/shady_x.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var pick_up: Label = $pick_up
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var torch: Sprite2D = $Torch
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -12,17 +13,27 @@ func _ready() -> void:
 	pick_up.visible = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("MoveLeft", "MoveRight")
+	
+	if direction > 0:
+		sprite_2d.flip_h = false
+	elif direction < 0:
+		sprite_2d.flip_h = true
+
+	if is_on_floor():
+		if direction == 0:
+			animation_player.play("Idle")
+		else: 
+			animation_player.play("Walking")
+	else: 
+		animation_player.play("Jumping")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
