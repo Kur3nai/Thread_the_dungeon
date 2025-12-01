55eea8591dbfea1036d42d7036a4f1768530c67e
Kay Faraday <yatagarasu163@gmail.com>
2025-12-01 17:56:50 +0800

    Adds the darkvision rune
    The darkvision rune temporarily gives the player darkvision, allowing
    them to see a majority of the room for a while before it goes back to
    being dark. Once the player collides with the rune, it disappears and
    the has_darkvision boolean in the player is made true, enlarging vision
    size to the vision radius, then slowly reducing it by the
    vision_reduction_speed until it goes back to a scale of 1.

--- thread/scripts/shady_x.gd
+++ thread/scripts/shady_x.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 @onready var pick_up: Label = $pick_up
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var light_area:PointLight2D = $LightArea;
 @onready var torch: Sprite2D = $Torch
+var has_darkvision:bool = false;
+@export var vision_radius:float = 8.0;
+var vision_size:Vector2 = Vector2(vision_radius, vision_radius);
+@export var vision_cooldown:float = 2.0;
+@export var vision_reduction_speed:float = 3.0;
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
@@ -40,7 +46,27 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if has_darkvision:
+		_activate_darkvision();
 	
+func _activate_darkvision():
+	light_area.scale = vision_size;
+	_darkvision_timer();
+
+func _deactivate_darkvision():
+	var tween = get_tree().create_tween()
+	tween.tween_property(light_area, "scale", Vector2(1,1), 1.0) # 1 second fade-out
+	tween.play()
+	has_darkvision = false
+
+func _darkvision_timer():
+	var timer:Timer = Timer.new();
+	add_child(timer)
+	timer.wait_time = vision_cooldown;
+	timer.one_shot = true;
+	timer.start();
+	timer.timeout.connect(_deactivate_darkvision);
+
 func show_message(text):
 	$pick_up.text = text
 	$pick_up.show()
