46e6363a9e8a0c2bfb327c7277dce419e21ef023
Kur3nai <darkserr0@gmail.com>
2025-12-02 23:30:25 +0800

    Runes

--- thread/scripts/runes.gd
+++ thread/scripts/runes.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var rune_type: String = "has_darkvision" # Default
+@export var rune_type: String = "has_darkvision" 
 
 func _on_pickup_range_entered(body: Node2D):
 	if(body.is_in_group("ShadyX")):
@@ -10,9 +10,9 @@ func _on_pickup_range_entered(body: Node2D):
 			"increase_speed":
 				Global.speed += 50
 			"increase_jump":
-				Global.Jump_velocity -= 50
+				Global.Jump_velocity -= 100
 			"increase_vision_radius":
-				body.vision_radius += 0.5
+				body.vision_radius += 5
 				if body.has_darkvision:
 					body.vision_size = Vector2(body.vision_radius, body.vision_radius)
 		queue_free()
