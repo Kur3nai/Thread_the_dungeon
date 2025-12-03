638da862fa3e9fe7586a5d5b48d9aefec367550f
Kay Faraday <yatagarasu163@gmail.com>
2025-12-03 22:44:33 +0800

    Fixes Platform
    :(

--- thread/scripts/vertical_platform_manager.gd
+++ thread/scripts/vertical_platform_manager.gd
@@ -5,23 +5,40 @@ extends Node2D
 @onready var end_point:Node2D = $End;
 @onready var platform:Node2D = $Platform;
 @export var is_activated:bool = true;
+@export var reverse: bool = false;
 var up_to_down:bool = true;
 @export var platform_speed:float = 1.0;
 
 func _ready() -> void: 
-	start_point.position.y = platform.position.y - move_height;
-	end_point.position.y = platform.position.y + move_height;
+	if reverse:
+		start_point.position.y = platform.position.y + move_height;
+		end_point.position.y = platform.position.y - move_height;
+	else:
+		start_point.position.y = platform.position.y - move_height;
+		end_point.position.y = platform.position.y + move_height;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_activated:
-		if up_to_down:
-			if platform.position.y < end_point.position.y:
-				platform.position.y += (platform_speed * delta);
-			else:
-				up_to_down = !up_to_down;
-		else: 
-			if platform.position.y > start_point.position.y:
-				platform.position.y -= (platform_speed * delta);
+		if reverse == false:
+			if up_to_down:
+				if platform.position.y < end_point.position.y:
+					platform.position.y += (platform_speed * delta);
+				else:
+					up_to_down = !up_to_down;
 			else: 
-				up_to_down = !up_to_down;
+				if platform.position.y > start_point.position.y:
+					platform.position.y -= (platform_speed * delta);
+				else: 
+					up_to_down = !up_to_down;
+		else:
+			if up_to_down:
+				if platform.position.y > end_point.position.y:
+					platform.position.y -= (platform_speed * delta)
+				else:
+					up_to_down = !up_to_down;
+			else:
+				if platform.position.y < start_point.position.y:
+					platform.position.y += (platform_speed * delta)
+				else:
+					up_to_down = !up_to_down;
