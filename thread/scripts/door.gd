1ac1b9f452fa2f281a8fb47f5dea329993c85d94
Pokijoki <liangxuan0311@gmail.com>
2025-12-03 20:41:24 +0800

    Update door.gd

--- thread/scripts/door.gd
+++ thread/scripts/door.gd
@@ -23,23 +23,29 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ShadyX"):
-		Global.max_level+=1
 		var current_scene:String = get_tree().current_scene.name
 		if current_scene == "Level_1":
+			Global.max_level = 2
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_2.tscn")
 		elif current_scene == "Level_2":
+			Global.max_level = 3
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_3.tscn")
 		elif current_scene == "Level_3": 
+			Global.max_level = 4
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_4.tscn")
 		elif current_scene == "Level_4":
+			Global.max_level = 5
 			AudioManager._on_scene_changed("2")
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_5.tscn")
 		elif current_scene == "Level_5":
+			Global.max_level = 6
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_6.tscn")
 		elif current_scene == "Level_6":
+			Global.max_level = 7
 			AudioManager._on_scene_changed("3")
 			get_tree().change_scene_to_file("res://scene/Level Design/Level_7.tscn")
 		elif current_scene == "Level_7":
+			Global.max_level = 8
 			get_tree().change_scene_to_file("res://scene/Level Design/Main_Menu.tscn");
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
