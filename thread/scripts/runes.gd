9db73751feb2a8e41a9497f85634cecc64b0d966
Kur3nai <darkserr0@gmail.com>
2025-12-03 12:35:37 +0800

    Runes var Export

--- thread/scripts/runes.gd
+++ thread/scripts/runes.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 enum RuneType {HAS_DARKVISION, INCREASE_SPEED, INCREASE_JUMP, INCREASE_VISION_RADIUS}
-
-@export var rune_type: RuneType = RuneType.HAS_DARKVISION  
+@export var rune_type: RuneType = RuneType.HAS_DARKVISION
+@export var speed_increase: int = 50
+@export var jump_increase: int = -100
+@export var vision_increase: float = 5.0
+@onready var light_area: PointLight2D = $LightArea
 
 func _on_pickup_range_entered(body: Node2D):
 	if(body.is_in_group("ShadyX")):
@@ -18,3 +21,9 @@ func _on_pickup_range_entered(body: Node2D):
 				if Global.Has_Darkvision: 
 					body.vision_size = Vector2(body.vision_radius, body.vision_radius)
 		queue_free()
+
+func _process(delta: float) -> void:
+	if Global.letter_visible:
+		light_area.visible = false;
+	else:
+		light_area.visible = true;
--- thread/scripts/trapdoor_manager.gd
+++ thread/scripts/trapdoor_manager.gd
@@ -1,21 +1,15 @@
 extends Node2D
 
-@onready var start_point:Node2D = $Start;
-@onready var end_point:Node2D = $End;
-@onready var trapdoor:Node2D = $Trapdoor;
-@export var end_height:float = 20;
-@export var is_activated:bool = false;
-@export var trapdoor_speed:float = 3.0;
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var collider: CollisionShape2D = $Area2D/CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	end_point.position.y = -end_height;
+var is_open = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if is_activated: 
-		if trapdoor.position.y > end_point.position.y: 
-			trapdoor.position.y -= trapdoor_speed * delta;
-	else:
-		if trapdoor.position.y < start_point.position.y:
-			trapdoor.position.y += trapdoor_speed * delta;
+func open_trapdoor():
+	if is_open:
+		return
+		
+	is_open = true
+	anim.play("open") 
+	await anim.animation_finished
+	collider.disabled = true
