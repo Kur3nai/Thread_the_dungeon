8a42308b1b29fb11304d386af3089dffc5d4e2de
Pokijoki <liangxuan0311@gmail.com>
2025-12-03 21:47:01 +0800

    add pause menu

---
+++ thread/scene/Level Design/panel.gd
@@ -0,0 +1,16 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
