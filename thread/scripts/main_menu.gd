6e44323afda4b26086aaf151ec823c59e11a0e45
Pokijoki <liangxuan0311@gmail.com>
2025-12-03 20:12:17 +0800

    play button now goes to level select, level select button have numbers

--- thread/scripts/main_menu.gd
+++ thread/scripts/main_menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/Level Design/Level_1.tscn")
+	get_tree().change_scene_to_file("res://scene/Level Design/Level_Select.tscn")
 
 
 func _on_options_pressed() -> void:
