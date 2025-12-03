e0f1ddc9d24e7d7209c78181ca38a0810f1cc8e3
Kay Faraday <yatagarasu163@gmail.com>
2025-12-03 21:55:23 +0800

    Fixes pause menu
    Pause menu now is always working even when the get_tree().paused is true

--- thread/scene/Level Design/panel.gd
+++ thread/scene/Level Design/panel.gd
@@ -3,14 +3,11 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	visible = false
+	self.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		visible = true
-		get_tree().paused = true
+		self.visible = !self.visible
+		get_tree().paused = true;
